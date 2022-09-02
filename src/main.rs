ccacf9b55ee811717face8bf3bf58b29dd6dcc2d
Alex <AlexFasMod@mail.ru>
2022-09-02 20:04:00 +0500

    Условие if else

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 3;
+
+    if number % 4 == 0{
+        println!("число делится на 4");
+    }else if number % 3 == 0{
+        println!("число делится на 3");
+    }else if number % 2 == 0{
+        println!("число делится на 2");
+    }else{
+        println!("Число не делится на 4, 3 и 2");
+    }
+
+    let condition = true;
+    let number = if condition {
+        5
+    }else {
+        6
+    };
+    println!("number = {}", number);
 }
